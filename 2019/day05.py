ad2e17a52931d5d097d81454ecea2673982c5e79
Jonathan Whitall <jwhitall@Jonathans-MacBook-Pro.local>
2019-12-11 08:17:10 -0600

    made day 5 compatible with latest intcode

--- 2019/day05.py
+++ 2019/day05.py
@@ -1,4 +1,5 @@
-import intcode
+from intcode import run
+from intcode import ProgramState
 
 # Initialize: open file, turn all op codes into integers
 with open('day05.txt') as f:
@@ -10,12 +11,12 @@ with open('day05.txt') as f:
 myOps = ops.copy()
 
 latestOutput = None
-state = intcode.run(myOps, [1])
+state = run(ProgramState(myOps), [1])
 while not state.done:
     latestOutput = state.output
     # print (latestOutput)
-    state = intcode.run(myOps, [], state.address)
+    state = run(state, [])
 print ("Part One: %s" % latestOutput)
 
 myOps = ops.copy()
-print ("Part Two: %s" % intcode.run(myOps, [5]).output)
+print ("Part Two: %s" % run(ProgramState(myOps), [5]).output)
